df089489827b5abeef51b0588ab8003b05d7aa09
russelleh <russell.west.eh@gmail.com>
2015-07-28 09:34:40 -0600

    It can return it's created time as string

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,10 @@
 class Customer
+  attr_reader :id, :first_name, :last_name, :created_at
+
   def initialize id, first_name, last_name, created_at, updated_at
+    @id = id.to_i
+    @first_name = first_name
+    @last_name = last_name
+    @created_at = Time.parse(created_at)
   end
 end
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -7,4 +7,34 @@ class CustomerTest < Minitest::Test
 
     assert customer
   end
+
+  def test_it_can_return_its_own_id
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.id, 1
+  end
+
+  def test_it_can_return_its_first_name
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.first_name, "Joey"
+  end
+
+  def test_it_can_return_its_last_name
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.last_name, "Ondricka"
+  end
+
+  def test_it_can_return_its_created_at_time_as_timestamp
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.created_at.class, Time
+  end
+
+  def test_it_can_return_its_created_at_time_as_string
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.created_at.to_s, "2015-03-27 14:15:09 UTC"
+  end
 end
