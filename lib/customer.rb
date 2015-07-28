e4f2ffcf013896d0a0f38e8d00655eaf1b2e39e2
russelleh <russell.west.eh@gmail.com>
2015-07-28 09:35:42 -0600

    It can return it's updated at time as timestamp

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,11 @@
 class Customer
-  attr_reader :id, :first_name, :last_name, :created_at
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
 
   def initialize id, first_name, last_name, created_at, updated_at
     @id = id.to_i
     @first_name = first_name
     @last_name = last_name
     @created_at = Time.parse(created_at)
+    @updated_at = Time.parse(updated_at)
   end
 end
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -37,4 +37,10 @@ class CustomerTest < Minitest::Test
 
     assert_equal customer.created_at.to_s, "2015-03-27 14:15:09 UTC"
   end
+
+  def test_it_can_return_its_updated_at_time_as_timestamp
+    customer = Customer.new("1", "Joey", "Ondricka", "2015-03-27 14:15:09 UTC", "2012-03-27 14:54:09 UTC")
+
+    assert_equal customer.updated_at.class, Time
+  end
 end
