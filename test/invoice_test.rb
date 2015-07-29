3cabc1efdc3c2a6ac9e10fd38e06eda685f1a8ef
russelleh <russell.west.eh@gmail.com>
2015-07-29 09:45:56 -0600

    Fix order of tests in invoice test

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -17,25 +17,25 @@ class InvoiceTest < Minitest::Test
   def test_it_has_a_customer_id
     invoice = Invoice.new nil, {customer_id: "1"}
 
-    assert_equal invoice.customer_id, "1"
+    assert_equal "1", invoice.customer_id
   end
 
   def test_it_has_a_merchant_id
     invoice = Invoice.new nil, {merchant_id: "123"}
 
-    assert_equal invoice.merchant_id, "123"
+    assert_equal "123", invoice.merchant_id
   end
 
   def test_it_has_status
     invoice = Invoice.new nil, {status: "shipped"}
 
-    assert_equal invoice.status, "shipped"
+    assert_equal "shipped", invoice.status
   end
 
   def test_it_has_created_at_time
     invoice = Invoice.new nil, {created_at: "2012-03-25 09:54:09 UTC"}
 
-    assert_equal invoice.created_at.to_s, "2012-03-25 09:54:09 UTC"
+    assert_equal "2012-03-25 09:54:09 UTC", invoice.created_at.to_s
   end
 
   def test_created_at_time_is_time_class
