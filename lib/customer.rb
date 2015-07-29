ae162cb14bc5286750700c253996a0683a1520bd
russelleh <russell.west.eh@gmail.com>
2015-07-28 19:44:00 -0600

    Move customar methods to data instance class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,15 +1,5 @@
-class Customer
+require_relative 'data_instance'
+
+class Customer < DataInstance
   attr_reader :repository, :first_name, :last_name, :created_at, :updated_at
-
-  def initialize repository, hash
-    hash.each do |key, value|
-      instance_variable_set("@#{key}", parse(value)) unless key.nil?
-    end
-  end
-
-  def parse input
-    time = Time.parse(input) rescue nil
-    input = time if input == time.to_s
-    input
-  end
 end
---
+++ lib/data_instance.rb
@@ -0,0 +1,13 @@
+class DataInstance
+  def initialize repository, hash
+    hash.each do |key, value|
+      instance_variable_set("@#{key}", parse(value)) unless key.nil?
+    end
+  end
+
+  def parse input
+    time = Time.parse(input) rescue nil
+    input = time if input == time.to_s
+    input
+  end
+end
