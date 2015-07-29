66d84a8b33c37af62f12122e9ff70fdfdacb6f07
russelleh <russell.west.eh@gmail.com>
2015-07-29 17:33:46 -0600

    It loads invoice repository

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -9,4 +9,12 @@ class SalesEngineTest < Minitest::Test
 
     assert_kind_of CustomerRepository, engine.customer_repository
   end
+
+  def test_it_loads_invoice_repository
+    engine = SalesEngine.new
+
+    engine.startup
+
+    assert_kind_of InvoiceRepository, engine.invoice_repository
+  end
 end
