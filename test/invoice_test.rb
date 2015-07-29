62a0dadb36ae8a32b86d826408cdf145857257b0
russelleh <russell.west.eh@gmail.com>
2015-07-29 09:31:10 -0600

    It is an invoice

---
+++ test/invoice_test.rb
@@ -0,0 +1,10 @@
+require_relative 'test_helper'
+require_relative '../lib/invoice'
+
+class InvoiceTest < Minitest::Test
+  def test_it_is_an_invoice
+    invoice = Invoice.new
+
+    assert_kind_of Invoice, invoice
+  end
+end
