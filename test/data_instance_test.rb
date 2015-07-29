c8eb4e1124415124618eb7d1207231a945591779
russelleh <russell.west.eh@gmail.com>
2015-07-29 17:09:49 -0600

    Spelling

--- test/data_instance_test.rb
+++ test/data_instance_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 require_relative '../lib/data_instance'
 
 class DataInstanceTest < Minitest::Test
-  def test_has_a_list_of_referrable_attributes
+  def test_has_a_list_of_referable_attributes
     data = DataInstance.new nil, {}
 
     assert_kind_of Hash, data.referable
