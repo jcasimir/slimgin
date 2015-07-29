74824857f4ff614888346be7af8ddea89c3d63ce
russelleh <russell.west.eh@gmail.com>
2015-07-29 17:31:32 -0600

    It loads dependencies

---
+++ lib/sales_engine.rb
@@ -0,0 +1,10 @@
+class SalesEngine
+  attr_accessor :merchant_repository, :invoice_repository, :item_repository, :invoice_item_repository, :customer_repository, :transaction_repository
+  def startup
+    locations = {
+      customer_repository: "./fixtures/customers_head.csv",
+      invoice_repository: "./fixutres/invoices_head.csv",
+    }
+    @customer_repository = CustomerRepository.new locations[:customer_repository]
+  end
+end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,4 +2,11 @@ require_relative 'test_helper'
 require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
+  def test_it_loads_dependencies
+    engine = SalesEngine.new
+
+    engine.startup
+
+    assert_kind_of CustomerRepository, engine.customer_repository
+  end
 end
