70415486fc0875858bc5a83a3e31bbc6a2b08d13
russelleh <russell.west.eh@gmail.com>
2015-07-28 19:45:36 -0600

    Moved customer repository methods to repository class

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,24 +1,6 @@
 require 'csv'
+require_relative 'repository'
 
-class CustomerRepository
+class CustomerRepository < Repository
   attr_accessor :filename, :database
-
-  def initialize filename
-    @filename = File.new(filename)
-    @database = load_db
-  end
-
-  def load_db
-    db = {}
-    CSV.foreach filename, :headers => true do |row|
-      id = row.fields[0]
-      row_as_hash = row.to_hash.tap{|x| x.delete("id")}
-      db[id] = Customer.new(self, row_as_hash)
-    end
-    db
-  end
-
-  def id id_number
-    database[id_number]
-  end
 end
